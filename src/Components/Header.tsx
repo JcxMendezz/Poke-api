bc4eb540d67b60641b692e49da1aa5cc1f0ce9d6
Juan Camilo Méndez <juanmendez17t@gmail.com>
2024-10-29 12:38:00 -0500

    Refactor: Header Component

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,23 +1,27 @@
 import styles from "./header.module.css";
+import React from "react";
 // Need a plugin for typescript to use module css
 
 type HeaderProps = {
-  query: string;
-  setQuery: (query: string) => void;
+    query: string;
+    setQuery: (query: string) => void;
 };
 
-const Header = ({ query, setQuery }: HeaderProps) => {
-  return (
-    <header className={styles.header}>
-      <input
-        className={styles.input}
-        value={query}
-        placeholder="Busca un Pokemon"
-        onChange={(event) => setQuery(event.target.value.trim())}
-        type="text"
-      />
-    </header>
-  );
+const Header = ({query, setQuery}: HeaderProps) => {
+    return (
+        <header className={styles.header}>
+            <h1 className={styles.title}>
+                Pokedex Finder
+            </h1>
+            <input
+                className={styles.input}
+                value={query}
+                placeholder="Busca Pokemons"
+                onChange={(event) => setQuery(event.target.value.trim())}
+                type="text"
+            />
+        </header>
+    );
 };
 
 export default Header;
--- src/Components/LoadingScreen.tsx
+++ src/Components/LoadingScreen.tsx
@@ -1,11 +1,13 @@
 import Pokedex from "../assets/pokedex.png";
 import styles from "./loadingScreen.module.css";
+import React from "react";
 
 function LoadingScreen() {
     return (
         <div className={styles.loadingScreen}>
-            <img className={styles.loadingScreenIcon} src={Pokedex} alt="Pokedex" />
+            <img className={styles.loadingScreenIcon} src={Pokedex} alt="Pokedex"/>
         </div>
     );
 }
+
 export default LoadingScreen;
